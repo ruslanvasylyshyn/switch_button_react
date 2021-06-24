6e59326464f205b8a6ce0d3d61a69441976b97c3
Ruslan Vasylyshyn <r.y.vasylyshyn@gmail.com>
2021-06-24 09:41:56 +0300

    Updates

---
+++ precache-manifest.8dfb935bd177aa6e60c50efcd16089f4.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "47a154429fa76f75b100e7268714c89b",
+    "url": "./index.html"
+  },
+  {
+    "revision": "6eb2ba5af0f1b071f4cc",
+    "url": "./static/css/main.fbd9299c.chunk.css"
+  },
+  {
+    "revision": "e698f19df3e51611492c",
+    "url": "./static/js/2.61910ce2.chunk.js"
+  },
+  {
+    "revision": "570d362d673dab785e62d2b8563e1118",
+    "url": "./static/js/2.61910ce2.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "6eb2ba5af0f1b071f4cc",
+    "url": "./static/js/main.f585fcca.chunk.js"
+  },
+  {
+    "revision": "bf5dfa9a4d75f6a930cf",
+    "url": "./static/js/runtime-main.816c614f.js"
+  },
+  {
+    "revision": "9478c1c1fc5b30cf26819010e24926d9",
+    "url": "./static/media/yotpo.9478c1c1.png"
+  }
+]);
